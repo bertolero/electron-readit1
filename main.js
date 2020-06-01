6020d922329dae9971282a7fca5188f51ea35666
Bertol <bertol.pinheiro@gmail.com>
2020-06-01 15:45:43 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,52 @@
+/*
+Need transpilation
+import {app, BrowserWindow} from 'electron';
+import windowStateKeeper from 'electron-window-state';
+ */
+const {app, BrowserWindow} = require('electron')
+const windowStateKeeper = require('electron-window-state')
+
+function createWindow() {
+
+    const state = windowStateKeeper({
+        defaultWidth: 500,
+        defaultHeight: 650
+    });
+
+    let mainWindow = new BrowserWindow({
+        x: state.x,
+        y: state.y,
+        width: state.width,
+        height: state.height,
+        minWidth: 350,
+        minHeight: 300,
+        maxWidth: 650,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadFile('renderer/main.html');
+
+    mainWindow.webContents.openDevTools();
+
+    state.manage(mainWindow);
+
+    mainWindow.on('close', () => {
+        mainWindow = null;
+    });
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
